a4c0cf13b0b6ae0637f47815c817222d2be0374e
Hervé Cauwelier <herve@itaapy.com>
2009-02-25 15:46:31 +0100

    0006036: Accélère l'export général

--- root.py
+++ root.py
@@ -329,6 +329,29 @@ class Root(BaseRoot):
     export__access__ = 'is_admin'
     def export(self, context):
 
+        __cache__ = {}
+
+        def get_form_and_namespace(container, name):
+            if name not in __cache__:
+                form = container.get_object(name)
+                schema = form.get_schema()
+                namespace = form.get_namespace(context)
+                for key, value in namespace.items():
+                    field_def = schema.get(key)
+                    if field_def is not None:
+                        ftype = field_def[0]
+                        if ftype is Unicode:
+                               if value is not None:
+                                   value = (value.replace(u"€", u"eur")
+                                                 .replace(u'"', u'\\"')
+                                                 .replace(u"&quot;", u'\\"')
+                                                 .replace(u"'", u"\\'"))
+                               namespace[key] = value
+                __cache__[name] = (form, namespace)
+
+            return __cache__[name]
+
+
         def export_adr(container, output, context):
             """Ajout ALP - 27 nov 2007
             """
@@ -338,29 +361,18 @@ class Root(BaseRoot):
                             folder.get_handler('%s.metadata' % name)\
                                     .get_property('state') == 'public')]
             for name in names:
-                form = container.get_object(name)
-                schema = form.get_schema()
-                namespace = form.get_namespace(context)
-                query = (u'UPDATE adresse SET libelle1="%(field1)s",libelle2="%(field2)s",'
-                 u'local="%(field30)s",voie_num="%(field31)s",voie_type="%(field32)s",'
-                 u'voie_nom="%(field33)s",cpbiblio="%(field4)s",ville="%(field5)s",'
-                 u'cedexb="%(field6)s",directeu="%(field7)s",st_dir="%(field8)s",tele="%(field9)s",'
-                 u'fax="%(field10)s",mel="%(field11)s",www="%(field12)s",'
-                 u'intercom="%(field13)s",gestion="%(field14)s",'
-                 u'gestion_autre="%(field15)s" where code_bib=')
-
-                for key, value in namespace.items():
-                    field_def = schema.get(key)
-                    if field_def is not None:
-                        ftype = field_def[0]
-                        if ftype is Unicode:
-                               if value is not None:
-                                   value = value.replace(u"€", u"eur")
-                                   value = value.replace(u'"', u'\\"')
-                                   value = value.replace(u"&quot;", u'\\"')
-                                   value = value.replace(u"'", u"\\'")
-                               namespace[key] = value
-
+                form, namespace = get_form_and_namespace(container, name)
+                query = (u'UPDATE adresse SET libelle1="%(field1)s",'
+                         u'libelle2="%(field2)s",local="%(field30)s",'
+                         u'voie_num="%(field31)s",'
+                         u'voie_type="%(field32)s",'
+                         u'voie_nom="%(field33)s",cpbiblio="%(field4)s",'
+                         u'ville="%(field5)s",cedexb="%(field6)s",'
+                         u'directeu="%(field7)s",st_dir="%(field8)s",'
+                         u'tele="%(field9)s",fax="%(field10)s",'
+                         u'mel="%(field11)s",www="%(field12)s",'
+                         u'intercom="%(field13)s",gestion="%(field14)s",'
+                         u'gestion_autre="%(field15)s" where code_bib=')
                 query = query % namespace
                 query = query + name + ';'
                 output.write(query.encode('latin1') + '\n')
@@ -371,9 +383,9 @@ class Root(BaseRoot):
             cursor = connexion.cursor()
             folder = container.handler
             names = [name for name in container.get_names()
-                    if name.isdigit() and (
-                        folder.get_handler('%s.metadata' % name)\
-                                .get_property('state') == 'public')]
+                        if name.isdigit() and (
+                            folder.get_handler('%s.metadata' % name)\
+                                    .get_property('state') == 'public')]
             if len(names) == 0:
                 output.write(u'-- aucune bibliothèque exportée'.encode('latin1'))
                 return
@@ -411,23 +423,8 @@ class Root(BaseRoot):
 
             # (bm|bdp)07
             for name in names:
-                object = container.get_object(name)
-                schema = object.get_schema()
-                namespace = object.get_namespace(context)
-                # XXX copy/paste
-                for key, value in namespace.items():
-                    field_def = schema.get(key)
-                    if field_def is not None:
-                        ftype = field_def[0]
-                        if ftype is Unicode:
-                            if value is not None:
-                                value = value.replace(u"€", u"eur")
-                                value = value.replace(u'"', u'\\"')
-                                value = value.replace(u"&quot;", u'\\"')
-                                value = value.replace(u"'", u"\\'")
-                            namespace[key] = value
-                # XXX end
-                query = object.get_export_query(namespace)
+                form, namespace = get_form_and_namespace(container, name)
+                query = form.get_export_query(namespace)
                 output.write(query.encode('latin1') + '\n')
 
         output_path = join(getcwd(), 'exportscrib.sql')
